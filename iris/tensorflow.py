75bf0027d303b29a60f6ddf42074e30cbe120cb3
catsymptote <catsymptote@gmail.com>
2017-11-23 01:31:35 +0100

    iris project added.

---
+++ iris/tensorflow.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import numpy as np
+
+print(tf.__version__)
+
+"""
+from tensorflow.contrib.learn.python.learn.datasets import base
+
+# Data files.
+IRIS_TRAINING = "datasets/iris_training.csv"
+IRIS_TEST = "datasets/iris_test.csv"
+
+# Load datasets
+training_set = base.load_csv_with_header(   filename = IRIS_TRAINING,
+                                            features_dtype = np.float32,
+                                            target_dtype = np.int)
+
+test_set = base.load_csv_with_header(   filename = IRIS_TEST,
+                                        features_dtype = np._float32,
+                                        target_dtype = np.int)
+
+# Print results.
+print(training_set.data)
+print(test_set.data)
+"""
